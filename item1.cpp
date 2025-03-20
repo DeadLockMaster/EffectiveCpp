dc511a01c5312e5c97521b46e212c51e73e82645
Deadlockmaster <054488aa@gmail.com>
2025-03-21 09:32:44 +1300

    first time

---
+++ item1.cpp
@@ -0,0 +1,12 @@
+#include "item1.hpp"
+#include <iostream>
+int main(int argc, char const *argv[])
+{
+    /* code */
+    using namespace std;
+
+    cout<<"Hello World"<<endl;
+    
+    return 0;
+}
+
